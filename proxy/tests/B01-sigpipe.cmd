8156d34ea593c965168c3518b91fabd2722b9e23
Nonsens_ <116316168+whn22@users.noreply.github.com>
2024-09-12 11:20:15 +0800

    Delete proxy directory

--- proxy/tests/B01-sigpipe.cmd
+++
@@ -1,9 +0,0 @@
-# Test ability of proxy to handle SIGPIPE signal
-generate r1.txt 1k
-serve s1
-# Send SIGPIPE signal to proxy
-signal 
-fetch f1 r1.txt s1
-wait *
-check f1
-quit
--- proxy/tests/B13-post-error.cmd
+++
@@ -1,6 +0,0 @@
-serve s1
-generate random-text.txt 4K
-post-request f1 random-text.txt s1
-wait *
-check f1 501
-quit
